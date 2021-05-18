99d6ab479ed1da0360e89acb1cad64f8e2ec9cc0
Robert Wolff <robert.wolff@idealo.de>
2021-05-18 11:14:01 +0200

    remove form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import Counter from "./components/Counter";
 import Welcome from "./components/Welcome";
@@ -6,40 +6,13 @@ import Title from "./components/Title";
 import Congratulations from "./components/Congratulations";
 
 function App() {
-  const [message, setMessage] = useState(undefined);
-  const [fieldValue, setFieldValue] = useState("");
-
-  const handleChange = ({ target }) => {
-    setFieldValue(target.value);
-  };
-
-  const submitAnswer = () => {
-    if (fieldValue === "42") {
-      setMessage("correct");
-    } else {
-      setMessage(undefined);
-    }
-  };
-
   return (
     <div className="App">
       <header className="App-header">
         <Welcome />
         <Counter />
         <Title />
-
-        <label htmlFor="answer">What is the answer?</label>
-        <input
-          type="text"
-          name="answer"
-          onChange={handleChange}
-          value={fieldValue}
-        />
-        <button onClick={submitAnswer} name="submit">
-          Submit
-        </button>
-
-        <Congratulations message={message} />
+        <Congratulations message={undefined} />
       </header>
     </div>
   );
